9b964eb86efff3faa00133d1fb856a80ef54cdb3
William Summe <wlsumme@gmail.com>
2024-05-28 11:44:03 -0500

    done

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var games = new List<string> {"Pac-Man", "Space Invaders", "Galaga", "Donkey Kong", "Asteroids" };
+
+            var orderedGames = games.OrderBy(x => x.Length).ToList();
+
+            foreach (var x in orderedGames)
+            {
+                Console.WriteLine(x);
+            }
         }
     }
 }
